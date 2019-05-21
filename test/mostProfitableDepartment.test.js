e161c2cb153b1c47c64993c33b7bf5b35d71bbeb
axolalose <axolalose@gmail.com>
2019-05-21 11:53:49 +0200

    replying to review

--- test/mostProfitableDepartment.test.js
+++ test/mostProfitableDepartment.test.js
@@ -2,7 +2,7 @@ describe("mostProfitableDepartment function",function(){
     it("Should return highest profitable department ",function(){
         assert.deepEqual(mostProfitableDepartment(salesData),'outdoor')
     });
-    it("Should return least profitable department ",function(){
+    it("Should return lowest profitable department ",function(){
         assert.deepEqual(mostProfitableDepartment(salesData),'outdoor')
     });
 });
--- test/totalPhoneBill.test.js
+++ test/totalPhoneBill.test.js
@@ -1,8 +1,8 @@
 describe("totalPhoneBill function",function(){
-    it("Should return R3.40 from Phone  ",function(){
+    it("Should return R3.40 if a 'call' and an 'sms' is added  ",function(){
         assert.equal(totalPhoneBill("call, sms"),'R3.40')
     })
-    it("Should return R2.75 from Phone ",function(){
+    it("Should return R2.75 if a 'call' and an 'sms' is not added ",function(){
         assert.equal(totalPhoneBill("call"),"R2.75")
     })
 })
