ff961f5a5be4777ebf4f24249ff2922312477551
axolalose <axolalose@gmail.com>
2019-05-22 10:53:16 +0200

    replying to 4

--- test/findItemsOver.test.js
+++ test/findItemsOver.test.js
@@ -1,9 +1,9 @@
 describe("findItemsOver function",function(){
     it("Should return all the products that have a quantity higher than 20",function(){
-    assert.deepEqual(findItemsOver([ { name: 'pear', qty: 21 }] ,20),[ { name: 'pear', qty: 21 } ])
+    assert.deepEqual(findItemsOver([ { name: 'pear', qty: 21 }] ,20), [ { name: 'pear', qty: 21 } ])
     })
     it("Should return an empty array if the is no product that is lower than 20",function(){
-        assert.deepEqual(findItemsOver([ { name: 'pear', qty: 25 }] ,"")[ { name: 'pear', qty: 25 } ])
+        assert.deepEqual(findItemsOver([ { name: 'pear', qty: 15 }] ,20), [])
         }) 
 })
 
