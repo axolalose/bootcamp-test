0a9c7cddaaf61891858eeeb1fceab96efabf6427
axolalose <axolalose@gmail.com>
2019-05-23 15:56:05 +0200

    made changes2

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -1,8 +1,8 @@
 describe("countAllFromTown function",function(){
-    it("Should return true all the registration numbers in the string that is for that Town  ",function(){
+    it("Should return all the registration numbers in the string that is for that Town  ",function(){
         assert.deepEqual(countAllFromTown("CS 123","CS"),["CS 123"])
     })
-    it("Should return false if registration is not from Town  ",function(){ 
+    it("Should return all registration from Town",function(){ 
         assert.deepEqual(countAllFromTown("CY 123,CS 123,CY 543","CY"),["CY 123","CY 543"])
     })
 })
